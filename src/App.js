54143e83125fa0b655ad9d10b577737b09eec5e7
J3Technology <160076359+J3Technology@users.noreply.github.com>
2024-03-12 00:00:43 -0400

    adding onSquareClick props

--- src/App.js
+++ src/App.js
@@ -1,36 +1,34 @@
 import { useState } from 'react';
 
-function Square () {
-  const [num, setNum] = useState(null);
-  function handleClick(){
-    setNum('X')
-  }
+function Square ({value, onSquareClick}) {
   return (
-    <button 
-      className="square" 
-      onClick={handleClick}>
-        {num}
-        </button>)
+    <button className='square' onClick={onSquareClick}>
+      {value}
+      </button>
+  )
 }
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+
   return (
     <>
       <div className='board-row'>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} onSquareClick={handleClick}/>
+        <Square value={squares[1]} onSquareClick={handleClick}/>
+        <Square value={squares[2]} onSquareClick={handleClick}/>
       </div>
       <div 
       className='board-row'>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]} onSquareClick={handleClick}/>
+        <Square value={squares[4]} onSquareClick={handleClick}/>
+        <Square value={squares[5]} onSquareClick={handleClick}/>
       </div>
       <div className='board-row'>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]} onSquareClick={handleClick}/>
+        <Square value={squares[7]} onSquareClick={handleClick}/>
+        <Square value={squares[8]} onSquareClick={handleClick}/>
       </div>
     </>)
   
